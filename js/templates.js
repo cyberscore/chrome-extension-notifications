3b7d146650521cac10dfb03b1dfb3f526f12c82c
Ricardo Mendes <rokusu@gmail.com>
2013-02-18 22:44:58 +0000

    fixed non-working links

--- js/templates.js
+++ js/templates.js
@@ -24,7 +24,7 @@ function program5(depth0,data) {
   buffer += "\n";
   return buffer;}
 
-  buffer += "<header>\n  <div class=\"brand\">\n    Cyberscore Notifications\n  </div>\n  Welcome, <strong>";
+  buffer += "<header>\n  <div class=\"brand\">\n    <a href=\"http://cyberscore.me.uk\" target=\"_blank\">Cyberscore Notifications</a>\n  </div>\n  Welcome, <strong>";
   foundHelper = helpers.username;
   if (foundHelper) { stack1 = foundHelper.call(depth0, {hash:{},data:data}); }
   else { stack1 = depth0.username; stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1; }
@@ -41,17 +41,22 @@ function program5(depth0,data) {
   return buffer;});
 templates['notification-table'] = template(function (Handlebars,depth0,helpers,partials,data) {
   helpers = helpers || Handlebars.helpers; data = data || {};
-  var buffer = "", stack1, foundHelper, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, functionType="function", self=this;
+  var buffer = "", stack1, foundHelper, self=this, helperMissing=helpers.helperMissing, escapeExpression=this.escapeExpression, functionType="function";
 
 function program1(depth0,data) {
   
   var buffer = "", stack1, stack2, foundHelper;
-  buffer += "\n        <tr>\n            <td>";
+  buffer += "\n        <tr ";
+  stack1 = depth0.unread;
+  stack2 = {};
+  stack1 = helpers['if'].call(depth0, stack1, {hash:stack2,inverse:self.noop,fn:self.program(2, program2, data),data:data});
+  if(stack1 || stack1 === 0) { buffer += stack1; }
+  buffer += ">\n            <td><input type=\"checkbox\"></td>\n            <td>";
   stack1 = depth0.timestamp;
   stack2 = {};
   foundHelper = helpers.formatDate;
   stack1 = foundHelper ? foundHelper.call(depth0, stack1, {hash:stack2,data:data}) : helperMissing.call(depth0, "formatDate", stack1, {hash:stack2,data:data});
-  buffer += escapeExpression(stack1) + "</td>\n            <td><a href=\"";
+  buffer += escapeExpression(stack1) + "</td>\n            <td><a target=\"_blank\" href=\"";
   stack1 = depth0.game;
   stack1 = stack1 == null || stack1 === false ? stack1 : stack1.link;
   stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1;
@@ -59,7 +64,7 @@ function program1(depth0,data) {
   stack1 = depth0.game;
   stack1 = stack1 == null || stack1 === false ? stack1 : stack1.name;
   stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1;
-  buffer += escapeExpression(stack1) + "</a></td>\n            <td><a href=\"";
+  buffer += escapeExpression(stack1) + "</a></td>\n            <td><a target=\"_blank\" href=\"";
   stack1 = depth0.chart;
   stack1 = stack1 == null || stack1 === false ? stack1 : stack1.link;
   stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1;
@@ -67,14 +72,18 @@ function program1(depth0,data) {
   stack1 = depth0.chart;
   stack1 = stack1 == null || stack1 === false ? stack1 : stack1.name;
   stack1 = typeof stack1 === functionType ? stack1.apply(depth0) : stack1;
-  buffer += escapeExpression(stack1) + "<a></td>\n        </tr>\n      ";
+  buffer += escapeExpression(stack1) + "</a></td>\n        </tr>\n      ";
   return buffer;}
+function program2(depth0,data) {
+  
+  
+  return "class=\"unread\"";}
 
   buffer += "<div class=\"table-container\">\n<table>\n    <caption>";
   stack1 = {};
   foundHelper = helpers.fullType;
   stack1 = foundHelper ? foundHelper.call(depth0, depth0, {hash:stack1,data:data}) : helperMissing.call(depth0, "fullType", depth0, {hash:stack1,data:data});
-  buffer += escapeExpression(stack1) + "</caption>\n    <thead>\n        <tr>\n            <td>Date</td>\n            <td>Game</td>\n            <td>Chart</td>\n        </tr>\n    </thead>\n    <tbody>\n      ";
+  buffer += escapeExpression(stack1) + "</caption>\n    <thead>\n        <tr>\n            <td></td>\n            <td>Date</td>\n            <td>Game</td>\n            <td>Chart</td>\n        </tr>\n    </thead>\n    <tbody>\n      ";
   stack1 = {};
   stack1 = helpers.each.call(depth0, depth0, {hash:stack1,inverse:self.noop,fn:self.program(1, program1, data),data:data});
   if(stack1 || stack1 === 0) { buffer += stack1; }
